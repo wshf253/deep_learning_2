faf60c45dd18398dcc34fe627593f1aa4db0c24b
wshf253 <65922112+wshf253@users.noreply.github.com>
2023-10-21 23:13:10 +0900

    10/21 ch2 finished

---
+++ ch02/ppmi.py
@@ -0,0 +1,36 @@
+import sys
+sys.path.append("..")
+import numpy as np
+from common.util import preprocess, create_co_matrix, cos_similarity
+
+def ppmi(C, verbose=False, eps=1e-8):
+    M = np.zeros_like(C, dtype=np.float32)
+    N = np.sum(C)
+    S = np.sum(C, axis=0)
+    total = C.shape[0] * C.shape[1]
+    cnt = 0
+
+    for i in range(C.shape[0]):
+        for j in range(C.shape[1]):
+            pmi = np.log2(C[i, j] * N / (S[i]*S[j]) + eps)
+            M[i, j] = max(0, pmi)
+
+            if verbose:
+                cnt += 1
+                if cnt % (total//100 + 1) == 0:
+                    print("%.1f%% done" % (100*cnt/total))
+
+    return M
+
+text = "You say goodbye and I say hello."
+corpus, word_to_id, id_to_word = preprocess(text)
+vocab_size = len(word_to_id)
+C = create_co_matrix(corpus, vocab_size)
+W = ppmi(C, verbose=True)
+
+np.set_printoptions(precision=3)
+print("co-occurrence matrix")
+print(C)
+print("-"*50)
+print("PPMI")
+print(W)
