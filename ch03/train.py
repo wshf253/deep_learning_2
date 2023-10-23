92b035731896c8a683c72c929321e0b17f4ca257
wshf253 <65922112+wshf253@users.noreply.github.com>
2023-10-23 17:29:20 +0900

    10/23 ch3 finished

--- ch03/train.py
+++ ch03/train.py
@@ -3,6 +3,7 @@ sys.path.append("..")
 from common.trainer import Trainer
 from common.optimizer import Adam
 from simple_cbow import SimpleCBOW
+from simple_skip_gram import SimpleSkipGram
 from common.util import preprocess, create_contexts_target, convert_one_hot
 
 window_size = 1
@@ -18,7 +19,8 @@ contexts, target = create_contexts_target(corpus, window_size)
 target = convert_one_hot(target, vocab_size)
 contexts = convert_one_hot(contexts, vocab_size)
 
-model = SimpleCBOW(vocab_size, hidden_size)
+#model = SimpleCBOW(vocab_size, hidden_size)
+model = SimpleSkipGram(vocab_size, hidden_size)
 optimizer = Adam()
 trainer = Trainer(model, optimizer)
 
